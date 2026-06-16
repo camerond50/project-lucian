3c7a2e7f1cdcd45a38b1a0521ca0624deb99c63e
camerond50 <camerond50@gmail.com>
2026-06-16 08:20:25 -0500

    Expose analytics export API to renderer

--- src/preload/index.js
+++ src/preload/index.js
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('lucianApi', {
   sendPrompt: (payload) => ipcRenderer.invoke('chat:send', payload),
+  exportAnalytics: () => ipcRenderer.invoke('analytics:export'),
   saveKey: (provider, keyValue) =>
     ipcRenderer.invoke('keys:set', provider, keyValue),
   keyStatus: () => ipcRenderer.invoke('keys:status')
