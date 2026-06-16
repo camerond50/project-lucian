bd892d477821a904d6f877157982bbe074016ffe
camerond50 <camerond50@gmail.com>
2026-06-16 18:19:08 -0500

    Expose analytics persistence controls

--- src/preload/index.js
+++ src/preload/index.js
@@ -3,6 +3,10 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('lucianApi', {
   sendPrompt: (payload) => ipcRenderer.invoke('chat:send', payload),
   exportAnalytics: () => ipcRenderer.invoke('analytics:export'),
+  analyticsPersistenceStatus: () =>
+    ipcRenderer.invoke('analytics:persistence:status'),
+  setAnalyticsPersistence: (enabled) =>
+    ipcRenderer.invoke('analytics:persistence:set', enabled),
   saveKey: (provider, keyValue) =>
     ipcRenderer.invoke('keys:set', provider, keyValue),
   keyStatus: () => ipcRenderer.invoke('keys:status')
